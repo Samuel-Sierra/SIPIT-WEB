43e7e369c1ab8c77222528020db19643770c404b
Samuel-Sierra <bocalin@hotmail.com>
2024-12-07 21:29:04 -0600

    quite try

--- main.py
+++ main.py
@@ -16,21 +16,17 @@ def home():
 @app.post('/comandos')
 def comandos(data: Comando):
     texto = data.texto
-    try:
-        respuesta = cn.CrearTarea(texto)
-        
-        content=""
-        if respuesta.status_code == 200:
-            content = "Concretado"
-            return JSONResponse(content=content, status_code=200)
-        else:
-            content = "No concretado"
-            return JSONResponse(content=content, status_code=respuesta.status_code)
-    except Exception as e:
 
-        ab = f"Error al crear tarea: {str(e)}"
-        content = ab+"Hubo un error al crear la tarea. Por favor, inténtalo de nuevo."
+    respuesta = cn.CrearTarea(texto)
+    
+    content=""
+    if respuesta.status_code == 200:
+        content = "Concretado"
+        return JSONResponse(content=content, status_code=200)
+    else:
+        content = "No concretado"
         return JSONResponse(content=content, status_code=respuesta.status_code)
+
 
     
     
