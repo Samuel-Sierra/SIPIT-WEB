341bca2a3945f43744c142466f31b8e3e179a29c
Samuel-Sierra <bocalin@hotmail.com>
2024-12-08 16:35:17 -0600

    bug

--- main.py
+++ main.py
@@ -16,18 +16,16 @@ def home():
 
 @app.post('/comandos/')
 def comandos(texto:str):
-    try:
-        respuesta, n = generarJsonComando(texto)
-        
-        if respuesta.status_code == 200:
-            content={"respuesta":n}
-            return JSONResponse(content=content, status_code=200)
-        else:
-            content={"respuesta":n}
-            return JSONResponse(content=content, status_code=respuesta.status_code)
-    except Exception as e:
-        ab = f"Error en {str(e)}"
-        return ab
+    #respuesta, n = generarJsonComando(texto)
+    n = generarJsonComando(texto)
+    return JSONResponse(content=n, status_code=500    )
+    #if respuesta.status_code == 200:
+    #    content={"respuesta":n}
+    #    return JSONResponse(content=content, status_code=200)
+    #else:
+    #    content={"respuesta":n}
+    #    return JSONResponse(content=content, status_code=respuesta.status_code)
+    
 
 
 @app.post('/minutatxt/')
