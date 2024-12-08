188ca5981318f063a8e24e534ac9e8c0ab554c06
Samuel-Sierra <bocalin@hotmail.com>
2024-12-07 20:06:00 -0600

    cambie a que recibiera un json

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from fastapi import FastAPI
 from notion import comandosNotion
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 cn = comandosNotion()
 app = FastAPI()
+
+class Comando(BaseModel):
+    texto: str
 
 @app.get('/')
 def home():
     return "Hola mundo!"
 
-@app.get('/comandos/{texto}')
-def comandos(texto : str):
+@app.post('/comandos/')
+def comandos(data: Comando):
+    texto = data.get("texto")
     respuesta = cn.CrearTarea(texto)
+    
     content=""
     if respuesta.status_code == 200:
         content = "Concretado"
@@ -19,5 +25,6 @@ def comandos(texto : str):
     else:
         content = "No concretado"
         return JSONResponse(content=content, status_code=respuesta.status_code)
+    
     
     
