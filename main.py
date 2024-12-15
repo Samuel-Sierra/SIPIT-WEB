f8e10b590a008959362048211f05f925393d2a94
Samuel-Sierra <bocalin@hotmail.com>
2024-12-15 12:46:39 -0600

    bugcito

--- main.py
+++ main.py
@@ -62,8 +62,9 @@ def minuta_resumen(texto_minuta:str):
     try:
         #n = generarJsonMinuta (texto_minuta)
         resumen_minuta = generarResumenMinuta (texto_minuta)
+        collection = {"resumen":resumen_minuta}
         db = get_db()
-        db.minutasResumen.insert_one("resumen:"+resumen_minuta)
+        db.minutasResumen.insert_one(collection)
         
         content={"respuesta":"Se recibió la minuta con éxito"+", se generó el resumen"}
         return JSONResponse(content=content, status_code=202)
