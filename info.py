8f8ebc273b6d35d9c673780fc9b12f2401d43baa
Samuel-Sierra <bocalin@hotmail.com>
2024-12-18 14:14:55 -0600

    cambios en consultar 7

--- info.py
+++ info.py
@@ -1,68 +1,66 @@
 from notion import ComandosNotion
+import json
 def obtenerTodo():
     cn = ComandosNotion()
     todo = []
     # Obtener todos los nombres de los proyectos
+    # Obtener todos los nombres de los proyectos
     nombres_proyectos = cn.obtener_nombres("proyecto")
+    print("Proyectos:", nombres_proyectos)
     for name in nombres_proyectos:
         item = {
             "tipo": "proyecto",
             "accion": "consultar",
             "nombre": name
         }
-        id_proyecto = cn.obtener_id_por_nombre(name,"proyecto")
-        
         datos_previos = cn.consultar_proyecto(item, False)
         datos_previos = datos_previos.get("results")[0]
         n = datos_previos.get("properties")
-        
         a = cn.extraer_datos_proyecto(n)
-        
-        a["id_proyecto"] = id_proyecto
-        a["tipo"] = "proyecto"
         todo.append(a)
+           
 
-    # Obtener todos los nombres de los sprints
-    #nombres_sprints = cn.obtener_nombres("sprint")
-    #print("Sprints:", nombres_sprints)
-    #for name in nombres_sprints:
-    #    item = {
-    #        "tipo": "sprint",
-    #        "accion": "consultar",
-    #        "nombre": name
-    #    }
-    #    datos_previos = cn.consultar_sprint(item, False)
-    #    datos_previos = datos_previos.get("results")[0]
-    #    n = datos_previos.get("properties")
-    #    a = cn.extraer_datos_sprint(n)
-    #todo.append(a)
-
-    # Obtener todos los nombres de las tareas
-    nombres_tareas = cn.obtener_nombres("tarea")
-    for name in nombres_tareas:
+    nombres_sprints = cn.obtener_nombres("sprint")
+    print("Sprints:", nombres_sprints)
+    for name in nombres_sprints:
         item = {
-            "tipo": "tarea",
+            "tipo": "sprint",
             "accion": "consultar",
             "nombre": name
         }
-        datos_previos = cn.consultar_tarea(item, False)
+        datos_previos = cn.consultar_sprint(item, False)
         datos_previos = datos_previos.get("results")[0]
         n = datos_previos.get("properties")
-        a = cn.extraer_datos_tareas(n)
-        a["tipo"] = "tarea"
+        a = cn.extraer_datos_sprint(n)
         todo.append(a)
+            
+        # Obtener todos los nombres de las tareas
+        nombres_tareas = cn.obtener_nombres("tarea")
+        print("Tareas:", nombres_tareas)
+        for name in nombres_tareas:
+            item = {
+                "tipo": "tarea",
+                "accion": "consultar",
+                "nombre": name
+            }
+            datos_previos = cn.consultar_tarea(item, False)
+            datos_previos = datos_previos.get("results")[0]
+            n = datos_previos.get("properties")
+            a = cn.extraer_datos_tareas(n)
+            todo.append(a)
 
 # Obtener todos los nombres de las minutas
-  #  nombres_minutas = cn.obtener_nombres("minuta")
-  #  for name in nombres_minutas:
-  #      item = {
-  #          "tipo": "minuta",
-  #          "accion": "consultar",
-  ##          "nombre": name
-   #     }
-   ##     datos_previos = cn.consultar_minuta(item, False)
+    #nombres_minutas = cn.obtener_nombres("minuta")
+    #for name in nombres_minutas:
+    #    item = {
+    #        "tipo": "minuta",
+    #        "accion": "consultar",
+    #        "nombre": name
+    #    }
+    #    datos_previos = cn.consultar_minuta(item, False)
     #    datos_previos = datos_previos.get("results")[0]
     #    n = datos_previos.get("properties")
-     #   a = cn.extraer_datos_minuta(n)
-    ##todo.append(a)
+    #    a = cn.extraer_datos_minuta(n)
+    #    a["nombre_minuta"] = name
+    #   todo.append(a)
     return todo
